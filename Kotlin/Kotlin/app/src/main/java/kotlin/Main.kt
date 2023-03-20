8ff4311d3016ff68dbfca05f58ebc0816bb5e3fb
uripper <36463273+uripper@users.noreply.github.com>
2023-03-20 12:04:26 -0600

    not working

--- Kotlin/Kotlin/app/src/main/java/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-data class Person(val name: String, val age: Int)
-fun main() {
-    val person_one = Person("Alice", 29)
-    val person_two = Person("Bob", 31)
-    println(person_one)
-    println(person_two)
-    println(person_one == person_two)
-}
